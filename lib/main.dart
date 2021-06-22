56825577c5ee0b2fbececeb272e665f8afb0dc79
shunsukenoguchi <58984561+shunsukenoguchi@users.noreply.github.com>
2021-06-22 23:57:08 +0900

    カウントアップ機能のスタートとストップの作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,86 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final items = List<String>.generate(10000, (i) => "Item $i");
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primaryColor: Colors.white,
+        primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('会議費用計算ツール'),
-        ),
-        body: Container(
-          child: Column(),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Timer _timer;
+  void _incrementCounter() {
+    _timer = Timer.periodic(Duration(seconds: 1), _countUp);
+  }
+
+  void _countUp(Timer timer) {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _stopCounter() {
+    _timer.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            RaisedButton(
+              color: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              onPressed: _incrementCounter,
+              child: Icon(Icons.add),
+            ),
+            RaisedButton(
+              color: Colors.red,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              onPressed: _stopCounter,
+              child: Icon(Icons.stop),
+            ),
+          ],
         ),
       ),
+
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
